c081771447ed4262077d7d709c9265ecc0e62da1
Joseph Siddall <j.b.siddall@gmail.com>
2020-04-28 15:29:44 +0100

    progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,21 @@ module.exports = {
     title: `Gatsby Typescript Starter`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyBGD16UaMqhMxFN1hc6n43RhNNkVxxDWgE",
+          authDomain: "wordcloud-275516.firebaseapp.com",
+          databaseURL: "https://wordcloud-275516.firebaseio.com",
+          projectId: "wordcloud-275516",
+          storageBucket: "wordcloud-275516.appspot.com",
+          messagingSenderId: "208531002144",
+          appId: "1:208531002144:web:9f39dbe906a877d15abd45"
+        }
+      }
+    }
   ],
-}
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,120 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
+import ReactWordcloud from 'react-wordcloud';
+import * as lodash from 'lodash';
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
-interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-  }
-}
+import firebase from "gatsby-plugin-firebase";
+import "firebase/firestore";
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
-}
+import {useCallback, useEffect, useState} from "react";
+import {Button, InputGroup, H1, Colors, Intent, ControlGroup} from "@blueprintjs/core";
+import styled from 'styled-components';
 
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
+import 'normalize.css/normalize.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+const COLORS_OUT = ['#FC4445', '#3FEEE6', '#55bcc9', '#97caef', '#cafafe'];
+
+const options = {
+  colors: COLORS_OUT,
+  enableTooltip: true,
+  deterministic: true,
+  fontFamily: 'impact',
+  fontSizes: [30, 45, 60],
+  fontStyle: 'normal',
+  fontWeight: 'normal',
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0],
+  scale: 'sqrt',
+  spiral: 'archimedean',
+  transitionDuration: 1000,
+} as const;
+
+const PageWrapper = styled.div`
+  padding-left: 20%;
+  padding-right: 20%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${Colors.WHITE};
+`;
+
+const WordCloudWrapper = styled.div`
+  width: 100%;
+  height: 400px;
 `
+
+const ControlGroupStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const HorizontalPadding = styled.div`
+  width: 10px;
+`;
+
+export default (props: {}) => {
+  const [db] = useState(() => firebase.firestore());
+
+  const [results, setResults] = useState<Array<{ text: string, value: number }>>([]);
+  const [newWord, setNewWord] = useState<string>("");
+
+  const updateWordCloud = useCallback(() => {
+    (async () => {
+      const results = await db.collection("WordSubmission").get();
+      const words: Array<string> = lodash.map(results.docs, d => d.data()['word']);
+      const frequency: { [key: string]: number } = lodash.countBy(words);
+      const data = lodash.values(lodash.mapValues(frequency, (value, key) => ({
+        text: key,
+        value: value
+      })));
+      setResults(data);
+    })();
+  }, [setResults, db]);
+
+  const onTextChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewWord(e.target.value);
+  }, [setNewWord]);
+
+  const onAddClick = useCallback(() => {
+    (async () => {
+      console.log('adding word: ' + newWord);
+      const result = await db.collection("WordSubmission").add({
+        word: newWord
+      });
+      setNewWord('');
+      console.log('updating cloud');
+      updateWordCloud();
+    })();
+  }, [db, newWord]);
+
+  const resetAll = useCallback(() => {
+    (async () => {
+      const batch = db.batch();
+      const docs = await db.collection('WordSubmission').get();
+      docs.forEach(doc => batch.delete(doc.ref));
+      await batch.commit();
+      updateWordCloud();
+    })();
+  }, [db, updateWordCloud]);
+
+  useEffect(() => {
+    updateWordCloud();
+  }, [updateWordCloud]);
+
+  return (
+      <PageWrapper className="$pt-elevation-shadow-2">
+        <WordCloudWrapper className="$pt-elevation-shadow-2">
+          <ReactWordcloud options={options as any} words={results} />
+        </WordCloudWrapper>
+        <ControlGroup fill vertical={false}>
+          <InputGroup fill type="text" leftIcon="edit" value={newWord} onChange={onTextChange}/>
+          <Button icon="add" onClick={onAddClick} />
+          <Button icon="reset" onClick={resetAll} />
+        </ControlGroup>
+      </PageWrapper>
+    )
+}
